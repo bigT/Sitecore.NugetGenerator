1a97c5d4c2bcc906bd5aed7e9120ad09ad7aac39
Taras Alenin <taras.alenin@gmail.com>
2015-08-22 17:56:39 +1000

    Hardened the installer and fixed the module install path.

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ A PowerShell module that automates generation of private Sitecore NuGet packages
 ## Install
 To install the module on Windows, you need to open a command prompt as an Administrator, and run the following script:
 	
-	@powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{iex ((new-object net.webclient).DownloadString('https://rawgit.com/bigT/Sitecore.NugetGenerator/master/install.ps1'))}"`
+	@powershell -NoProfile -ExecutionPolicy unrestricted -Command "&{iex ((new-object net.webclient).DownloadString('https://rawgit.com/bigT/Sitecore.NugetGenerator/master/install.ps1'))}"
--- install.ps1
+++ install.ps1
@@ -1,6 +1,7 @@
 ﻿$homepath = Join-Path $env:HOMEDRIVE $env:HOMEPATH
-$modulepath = Join-Path $homepath "\Documents\WindowsPowerShell\ModulesSitecoreNugetGenerator\SitecoreNugetGenerator.psm1"
+$modulepath = Join-Path $homepath "\Documents\WindowsPowerShell\Modules\SitecoreNugetGenerator\SitecoreNugetGenerator.psm1"
 
 $webClient = New-Object System.Net.WebClient
 Write-Host "Downloading Sitecore Nuget generator module to $modulepath"
+New-Item -ItemType Directory -Force -Path (split-path $modulepath) > $null
 $webClient.DownloadFile('https://rawgit.com/bigT/Sitecore.NugetGenerator/master/SitecoreNugetGenerator/SitecoreNugetGenerator.psm1', $modulepath)
